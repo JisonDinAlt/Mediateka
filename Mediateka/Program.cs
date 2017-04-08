04a829058fff7d1e3336e00a3b791f37203a20f5
emercom <emercom@grodno.net>
2017-04-08 17:16:35 +0300

    init commit

---
+++ Mediateka/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                BasicFile file1 = new BasicFile("Картинка1", 100, "jpg");
+                file1.rewrite();
+                
+
+            }
+        }
+
+        public  class BasicFile
+        {
+
+            public string FileName
+            {
+                get;
+                protected set;
+            }
+
+            public double FileSize
+            {
+                get;
+                protected set;
+            }
+
+            public string FileType
+            {
+                get;
+                protected set;
+            }
+
+            public string FileUlr
+            {
+                get;
+                protected set;
+            }
+
+            public BasicFile(string N, double S, string T)
+            {
+                FileName = N;
+                FileSize = S;
+                FileType = T;
+
+            }
+            public void  rewrite()
+            {
+                Console.WriteLine("Название файла - {0}, размер файла - {1}, тип фала - {2} ", FileName, FileSize, FileType);
+
+            }
+        }
+
+        
+
+    }
+}
